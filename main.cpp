461a65ebb6d5b73a478449c2b1113dccab1ee98b
sgonzalezr96 <77748332+sgonzalezr96@users.noreply.github.com>
2021-10-03 11:57:37 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
-
+#include "utiles.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Edificios* edificios = new Edificios;
+    cargar_edificios(edificios);
+
+    Materiales* materiales = new Materiales;
+    cargar_materiales(materiales);
+
+    int opcion = 0;
+
+    mostrar_menu();
+    opcion = pedir_opcion();
+    validar_opcion_elegida(opcion);
+
+    while(opcion != 6){
+        procesar_opcion(edificios, materiales, opcion);
+        mostrar_menu();
+        opcion = pedir_opcion();
+        validar_opcion_elegida(opcion);
+    }
+
+    cerrar_edificios(edificios);
+    cerrar_materiales(materiales);
+    delete materiales;
+    delete edificios;
+
     return 0;
 }
